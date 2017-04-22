4ae326679be85855b000576161de495a169a49b2
Danboru <8danang8@gmail.com>
2017-04-22 10:43:35 +0700

    Percobaan ingatan

---
+++ app/src/main/java/id/eightstudio/danboru/practicefunction/Aktifitas/AktifatPertama.java
@@ -0,0 +1,15 @@
+package id.eightstudio.danboru.practicefunction.Aktifitas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.eightstudio.danboru.practicefunction.R;
+
+public class AktifatPertama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktifat_pertama);
+    }
+}
--- app/src/main/java/id/eightstudio/danboru/practicefunction/SplashScreen/SplashScreen.java
+++ app/src/main/java/id/eightstudio/danboru/practicefunction/SplashScreen/SplashScreen.java
@@ -1,5 +1,6 @@
 package id.eightstudio.danboru.practicefunction.SplashScreen;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,22 +11,26 @@ import android.view.WindowManager;
 import id.eightstudio.danboru.practicefunction.MainActivity;
 import id.eightstudio.danboru.practicefunction.R;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
+    //Tag logger
     private static final String TAG = "SplashScreen";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        //Fullscreen mode
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        //Thread untuk menjalankan activity
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
+                    //Set Thread duration
                     Thread.sleep(3000);
                 }catch (Exception e){
                     e.printStackTrace();
@@ -35,8 +40,7 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
+        //Menjalankan thred
         thread.start();
     }
-
 }
